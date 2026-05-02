a43563fdea0734c6ff424a9007058fbcbef01375
Jakub <jkoudy@seznam.cz>
2026-05-02 19:10:08 +0200

    fix: use serverExternalPackages and alias missing yahoo-finance2 dev dep

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['yahoo-finance2', 'nodemailer', 'web-push', 'rss-parser'],
+  serverExternalPackages: ['yahoo-finance2', 'nodemailer', 'web-push', 'rss-parser'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.resolve.alias['@gadicc/fetch-mock-cache/stores/fs.ts'] = false
+    }
+    return config
   },
 }
 module.exports = nextConfig
