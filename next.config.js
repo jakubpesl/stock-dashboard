431547f5d458fc5e6f8b4338d96cd27ba9ce3215
Jakub <jkoudy@seznam.cz>
2026-05-02 20:45:13 +0200

    fix: use Next.js webpack instance for NormalModuleReplacementPlugin

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 const path = require('path')
-const webpack = require('webpack')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['yahoo-finance2', 'nodemailer', 'web-push', 'rss-parser'],
-  webpack: (config) => {
+  webpack: (config, { webpack }) => {
     const stub = path.resolve(__dirname, 'lib/empty-stub.js')
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(/^@gadicc\/fetch-mock-cache/, stub),
